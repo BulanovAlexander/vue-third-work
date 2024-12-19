b3862a5c250da0b14bec2ba666d1daceb056a384
Буланов Александр <alexbulanovgoodmail@gmail.com>
2024-12-19 18:32:51 +0300

    Хранилище для счётчика

---
+++ demo/src/demo4/DemoCounter.vue
@@ -0,0 +1,29 @@
+<script setup>
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+import { useCounterStore } from "./store";
+
+const counterStore = useCounterStore();
+</script>
+
+<template>
+  <div class="counter">
+    <CounterButton
+      :disabled="counterStore.isDecrementDisabled"
+      @click="counterStore.decrement"
+    >
+      -
+    </CounterButton>
+
+    <CounterValue :value="counterStore.value" />
+
+    <CounterButton @click="counterStore.increment"> + </CounterButton>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
