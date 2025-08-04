8bcb86f86ee6b1b4922b12fdc26e361ddbb07721
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-08-04 12:41:42 +0300

    Добавление логики для компонента счётчика

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,9 +1,23 @@
+<script setup lang="js">
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const decrement = () => {
+  counter.value = counter.value - 1;
+};
+
+const increment = () => {
+  counter.value = counter.value + 1;
+};
+</script>
+
 <template>
   <div>
-    <button>-</button>
+    <button @click="decrement">-</button>
 
-    <span> Current value: 0 </span>
+    <span> Current value: {{ counter }} </span>
 
-    <button>+</button>
+    <button @click="increment">+</button>
   </div>
 </template>
