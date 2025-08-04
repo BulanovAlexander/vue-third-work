86990d3215c8609d423535c40b5a53a2b0a54458
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-08-04 12:08:31 +0300

    Добавление мок-данных в проект

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,6 +1,9 @@
+<script setup lang="js">
+import AppLayoutHeader from './AppLayoutHeader.vue'
+</script>
 <template>
-  <div class="app_layout">
-    <app-layout-header />
-    <slot />
-  </div>
+	<div class="app_layout">
+		<AppLayoutHeader />
+		<slot />
+	</div>
 </template>
