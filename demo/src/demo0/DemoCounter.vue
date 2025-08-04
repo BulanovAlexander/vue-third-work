52d97c0abf74919f903c28bc898341dab0cca9cd
Буланов Александр <alexbulanovgoodmail@gmail.com>
2025-08-04 15:33:46 +0300

    Стилизация компонента счётчика

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,23 +1,46 @@
 <script setup lang="js">
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const counter = ref(0);
+const counter = ref(0)
 
 const decrement = () => {
-  counter.value = counter.value - 1;
-};
+	counter.value = counter.value - 1
+}
 
 const increment = () => {
-  counter.value = counter.value + 1;
-};
+	counter.value = counter.value + 1
+}
 </script>
 
 <template>
-  <div>
-    <button @click="decrement">-</button>
+	<div class="counter">
+		<button @click="decrement">-</button>
 
-    <span> Current value: {{ counter }} </span>
+		<span> Current value: {{ counter }} </span>
 
-    <button @click="increment">+</button>
-  </div>
+		<button @click="increment">+</button>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+.counter {
+	display: flex;
+	align-items: center;
+
+	button {
+		margin: 0 10px;
+		font-size: 1.5rem;
+		width: 2rem;
+		height: 2rem;
+		border-radius: 50%;
+		border: none;
+		background: #dd0000;
+		color: white;
+		cursor: pointer;
+
+		&:hover {
+			background: #ff0000;
+		}
+	}
+}
+</style>
